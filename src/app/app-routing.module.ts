ce85b993b10dd875b8b6341902fac4d725b4f130
ericwafula <eric.wafula@moringaschool.com>
2021-08-26 00:21:42 +0300

    fixed login and signup forms

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateAlertComponent } from './create-alert/create-alert.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
   {path: '', component: LandingPageComponent},
-  {path: 'create-alert', component: CreateAlertComponent}
+  {path: 'create-alert', component: CreateAlertComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'signup', component:SignupComponent}
 ];
 
 @NgModule({
